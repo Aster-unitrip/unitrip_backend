abf8595934de6bc61d9864d980ff501bc71be592
parker <parker@unitrip.asia>
2021-11-17 00:11:05 +0800

    fix connection string

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', '/cloudsql/instance_name'),
+            'unix_socket' => env('DB_SOCKET', '/cloudsql/unitrip'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
