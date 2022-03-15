452952dc5d157c16aef6313849b7899062d23c0e
Parker <kalpusan.jan@gmail.com>
2022-03-15 18:48:00 +0800

    調整「交通工具」API 及欄位： 1. 將原有 "transportations" 分為 "car_rental_company" 及 "transportations"，裝車行資料以及車型資料 2. 建立 viehcles_with_company view 作為查詢內容 3. 調整 API 查詢 collection

---
+++ app/Http/Controllers/ComponentCarController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\RequestPService;
+
+use Validator;
+
+class ComponentCarController extends Controller
+{
+    private $requestService;
+
+    public function __construct(RequestPService $requestService)
+    {
+        $this->middleware('auth');
+        $this->requestService = $requestService;
+    }
+
+    public function list(Request $request)
+    {
+        // Handle filter content
+        $filter = json_decode($request->getContent(), true);
+        if (array_key_exists('page', $filter)) {
+            $page = $filter['page'];
+            unset($filter['page']);
+            if ($page <= 0) {
+                return response()->json(['error' => 'page must be greater than 0'], 400);
+            }
+            else{
+                $page = $page - 1;
+            }
+        }
+        else{
+            $page = 0;
+        }
+
+
+        // unset($filter['fee']);
+        // Company_type: 1, Query public components belong to the company
+        // Company_type: 2, Query all public components and private data belong to the company
+        $company_type = auth()->payload()->get('company_type');
+        $company_id = auth()->payload()->get('company_id');
+        if ($company_type == 1){
+            $filter['owned_by'] = auth()->user()->company_id;
+            $query_private = false;
+        }
+        else if ($company_type == 2){
+            $query_private = true;
+            $filter['is_display'] = true;
+        }
+        else{
+            return response()->json(['error' => 'company_type must be 1 or 2'], 400);
+        }
+
+        // Handle projection content
+        $projection = array(
+            "_id" => 1,
+            "transportation_rental_agency" => 1,
+            "model" => 1,
+            "passenger_seats" => 1,
+            "fee" => 1,
+            "imgs" => 1,
+            "tel" => 1,
+            "address" => array(
+                "\$concat" => array(
+                    "\$address_city",
+                    "\$address_town",
+                    "\$address"
+                )
+                ),
+            // "address_city" => 1,    // 可能得從公司資料撈
+            // "address_town" => 1,    // 可能得從公司資料撈
+            // "address" => 1,         // 可能得從公司資料撈
+            "private" => 1,
+        );
+        $result = $this->requestService->aggregate_facet('viehcles_with_company', $projection, $company_id, $filter, $page, $query_private);
+        return $result;
+    }
+}
