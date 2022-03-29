5bedb4d8da2d92bb0cab91e16755eb805a5e9889
AsterChang <80759991+asterchang96@users.noreply.github.com>
2022-03-29 17:16:57 +0800

    (修改)更改修改狀態

--- app/Http/Controllers/ItineraryGroupController.php
+++ app/Http/Controllers/ItineraryGroupController.php
@@ -719,7 +719,7 @@ class ItineraryGroupController extends Controller
         if(!$itinerary_group_order_data){
             return response()->json(['error' => "團行程沒有關聯的訂單"], 400);
         }
-        if($itinerary_group_order_data['document']['order_status'] !== "已成團" || $itinerary_group_order_data['document']['order_status'] !== "棄單"){
+        if($itinerary_group_order_data['document']['order_status'] !== "已成團" && $itinerary_group_order_data['document']['order_status'] !== "棄單"){
             return response()->json(['error' => "訂單狀態不是[已成團]或[棄單]不可更改付款狀態"], 400);
         }
 
