84ec2ef9a71485cce5894d530e9cd0ee519cc6f3
Aster <aster@unitrip.asia>
2022-04-11 16:57:50 +0800

    (測試)

--- routes/api.php
+++ routes/api.php
@@ -127,11 +127,11 @@ Route::group(['middleware'=>'api', 'prefix'=>'passengers'], function($router){
     Route::post('/', [PassengerController::class, 'edit']);
 });
 
-Route::group(['middleware'=>'api', 'prefix'=>'dm'], function($router){ //DM系統
+/* Route::group(['middleware'=>'api', 'prefix'=>'dm'], function($router){ //DM系統
     Route::get('/setting/{id}', [DMController::class, 'get_dm_setting']); //取得dm設定資訊
     Route::get('/group-itinerary/{id}', [DMController::class, 'get_dm_group_itinerary']); //取得團行程放入dm
     Route::middleware('auth')->post('/setting', [DMController::class, 'edit_dm_setting']); // 更改DM設定 [是否上架]
-});
+}); */
 
 // 行程
 Route::group(['middleware'=>'api', 'prefix'=>'itinerary'], function($router){
@@ -139,4 +139,10 @@ Route::group(['middleware'=>'api', 'prefix'=>'itinerary'], function($router){
     Route::post('/list', [ItineraryController::class, 'list']);
     Route::get('/{id}', [ItineraryController::class, 'get_by_id']);
     Route::post('/update', [ItineraryController::class, 'edit']);
+});
+
+Route::group(['middleware'=>'api', 'prefix'=>'dm'], function($router){ //DM系統
+    Route::get('/setting/{id}', [DMController::class, 'get_dm_setting']); //取得dm設定資訊
+    Route::get('/group-itinerary/{id}', [DMController::class, 'get_dm_group_itinerary']); //取得團行程放入dm
+    Route::middleware('auth')->post('/setting', [DMController::class, 'edit_dm_setting']); // 更改DM設定 [是否上架]
 });
