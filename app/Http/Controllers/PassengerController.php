e3c9e3b37ce12655d81311391a96a0a7a312f258
Aster <aster@unitrip.asia>
2022-05-11 15:10:19 +0800

    (修改)旅客

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -28,7 +28,7 @@ class PassengerController extends Controller
             'id_number' => 'string|max:50',
             'passport_number' => 'string|max:50',
             'birthday' => 'required|string|date',
-            'is_vegetarian' => 'string',
+            'is_vegetarian' => ['nullable', new Boolean],
             'email' => 'email',
             'phone' => 'required|string',
             'job' => 'string|max:50',
