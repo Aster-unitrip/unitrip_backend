0b1f1c38a84b49a545557537e3e4a88f67ae8dcc
Aster <aster@unitrip.asia>
2022-06-09 12:37:04 +0800

    feat>US-908 fix passenger and add passenger_profile input data when using order's passenger

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -88,21 +88,26 @@ class PassengerController extends Controller
         if(array_key_exists('count', $order_data) && $order_data['count'] === 0){
             return response()->json(['error' => '沒有這筆訂單(no order_id)']);
         }
-        if($owned_by !== $order_data['owned_by']){
-            return response()->json(['error' => 'you are not an employee of this company.'], 400);
-        }
+        // if($owned_by !== $order_data['owned_by']){
+        //     return response()->json(['error' => 'you are not an employee of this company.'], 400);
+        // }
+
 
         // 判斷新增或是修改
         if(array_key_exists("_id", $validated)){ //修改
             $result = $this->requestService->update_one('passengers', $validated);
-            return $result;
         }
-        if(!array_key_exists("_id", $validated)){ //新增
-            $validated['is_representative'] = false;
-            $validated['owned_by'] = $owned_by;
-            $passengers_new = $this->requestService->insert_one('passengers', $validated);
-            return $passengers_new;
+        else if(!array_key_exists("_id", $validated)){ //新增
+            // 新增於 passenger_profile 中
+            $data_add_to_passenger_profile = $this -> ensure_passenger_profile_key($validated);
+            $content = $this->requestService->insert_one('passenger_profile', $data_add_to_passenger_profile);
+            $data_add_to_passenger_profile = json_decode($content->getContent(), true);
+
+            // 補上passenger_profile 該旅客id，新增於 passengers 中
+            $data_add_to_passenger = $this -> ensure_passengers_key($validated, $data_add_to_passenger_profile["inserted_id"]);
+            $result = $this->requestService->insert_one('passengers', $data_add_to_passenger);
         }
+        return $result;
     }
 
     public function list(Request $request)
@@ -117,4 +122,44 @@ class PassengerController extends Controller
         }
 
     }
+
+    // 刪除不必要的 key，避免回傳不該傳的資料
+    public function ensure_passenger_profile_key($data) {
+
+        // 修改姓名
+        $data['name'] = $this->ensure_name_key($data['name']);
+        $data['note'] = ""; // 備註
+        $data['mtp_number'] = ""; //台胞證
+        $data['visa_number'] = ""; // 簽證號碼
+        $data['owned_by'] = auth()->user()->company_id;
+        $data['updated_at'] = date('Y-m-d H:i:s');
+        $data['created_at'] = date('Y-m-d H:i:s');
+        unset($data['order_id']);
+        unset($data['is_representative']);
+
+        return $data;
+    }
+
+    // 刪除不必要的 key，避免回傳不該傳的資料
+    public function ensure_passengers_key($data, $inserted_id) {
+        $data['is_representative'] = false;
+        $data['owned_by'] = auth()->user()->company_id;
+        $data['passenger_profile_id'] = $inserted_id;
+        $data['updated_at'] = date('Y-m-d H:i:s');
+        $data['created_at'] = date('Y-m-d H:i:s');
+
+        return $data;
+    }
+
+    // 姓名轉換(過渡期)
+    public function ensure_name_key($name) {
+
+        if(gettype($name) === 'string') {
+            $name_changed['first_name'] = $name;
+            $name_changed['last_name'] = "";
+        }
+
+        return $name_changed;
+    }
+
 }
